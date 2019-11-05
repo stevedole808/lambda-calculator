72095712485ed1cf112e8fc818403dc550f11331
stevedole808 <stevedolelambda@gmail.com>
2019-11-05 11:10:09 -1000

    imported needed components

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {numbers} from '../../../data'
 
 const NumberButton = () => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>Numbers</button>
     </>
   );
 };
